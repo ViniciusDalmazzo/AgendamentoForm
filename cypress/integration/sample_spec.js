f4745ebcaa4acdb6e899326678e79c609c9ad04b
Bruno Mota Campos <bruno15mota@gmail.com>
2019-05-26 18:14:34 -0300

    Update sample_spec.js
    API tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,12 +2,19 @@ describe('Simple tests', () => {
     it('Should be open Agendamento', () => {
       cy.visit('/')
     }),
-    it('Should be return groups', () => {
+    it('Should be return grupos', () => {
         cy.request('http://controlequadra.herokuapp.com/api/grupo')
           .should((response) => {
             expect(response.status).to.eq(200)
             expect(response.body).to.be.an('array')
           })
           
-      })
+      }),
+      it('Should be return quadras', () => {
+        cy.request('http://controlequadra.herokuapp.com/api/quadra')
+          .should((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body).to.be.an('array')
+          })         
+      })   
   })
